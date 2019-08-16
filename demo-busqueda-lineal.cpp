47b296087ecbf1394a373ba17857aa581feb00f2
Daniel Ambort <dambort@gmail.com>
2019-08-15 21:12:58 -0300

    Add files via upload
    Subidos códigos de búsqueda lineal

---
+++ demo-busqueda-lineal.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include "enciclopedia.h"
+using namespace std; 
+
+
+int main(void){ 
+	int vector[] = { 2, 3, 4, 10, 40 }; 
+	int x = 4; 
+	int TL = sizeof(vector) / sizeof(vector[0]); 
+	
+	int resultado = BusquedaLineal(vector, TL, x); 
+	
+	if (resultado == -1)
+		cout << x << " no se encuentra en el vector";
+	else
+		cout << x << " se encuentra en la posicion " << resultado;
+	
+	return 0; 
+} 
+
---
+++ enciclopedia.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+
+int BusquedaLineal(int vector[], int TL, int x){ 
+	// La funcion retorna -1 si x no se encuentra en el vector
+	// en otro caso retorna el valor del subindice donde esta x
+	int i=0;
+	
+	while (i<TL) 
+		if (vector[i] == x) return i;
+		else i++;
+	
+	return -1;
+}
+
+
