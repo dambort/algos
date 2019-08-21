c0444c1b955073cc45092fbe53659af256d36289
Daniel Ambort <dambort@gmail.com>
2019-08-21 09:56:12 -0300

    Add files via upload
    Cada algoritmo en un archivo algoritmo.cpp, acompañado del .h correspondiente, por separado. Un archivo cliente "demo-algoritmo" para un uso ilustrativo del mismo.
    Hasta este momento dos algoritmos: búsqueda lineal y criba de eratóstenes.

---
+++ criba-eratostenes.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+#include "criba-eratostenes.h"
+using namespace std; 
+
+void cribaEratostenes (int n){
+	//Se crea un vector booleano de n+1 elementos
+	bool* primo;
+	primo = new bool [n+1];
+	
+	//Se inicializa cada elemento en "true"
+	for(int i=0; i<n+1; i++){
+		primo[i]=true;
+	}
+	
+	//Partiendo del primer primo (2) se asigna "false" a todos sus múltiplos
+	//Luego, se repite el proceso con el siguiente primo, es decir, el siguiente número con valor "true"
+	//El proceso se repite para todos los números p, tal que p*p<=n
+	for (int p=2; p*p<=n; p++){
+		if (primo[p] == true){
+			for (int i=p*p; i<=n; i+=p){
+				primo[i]=false;
+			}
+		}
+	}
+	
+	//Se imprimen en pantalla todos los primos encontrados
+	for (int p=2; p<=n; p++){
+		if(primo[p]){
+			cout << p << " ";
+		}
+	}
+	
+	delete [] primo;
+	primo=NULL;
+}
---
+++ demo-criba-eratostenes.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include "criba-eratostenes.h"
+using namespace std; 
+
+int main(void){ 
+	// programa cliente funcion cribaEratostenes
+	// el parametro n de la funcion indica hasta que valor contendrá el vector
+	// que se crea para poder determinar todos los números primos entre 1..n
+	
+	cribaEratostenes(100);
+	
+	return 0; 
+} 
+
