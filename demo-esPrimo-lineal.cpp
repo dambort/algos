35b3aef006b37747991452fa087383572527da73
MatiPalermo99 <51247617+MatiPalermo99@users.noreply.github.com>
2019-09-03 16:19:06 -0300

    Add files via upload

---
+++ demo-esPrimo-lineal.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include "esPrimo-lineal.h"
+using namespace std;
+
+int main (void) {
+	int n;
+	cout<<"Ingrese el numero que desea saber si es Primo: "<<endl;
+	
+	cin>>n;
+	
+	int comprobar=esPrimo(n);
+	
+	if(comprobar==1){
+		cout<<"El numero "<<n<<" es Primo"<<endl;
+	}else{
+		cout<<"El numero "<<n<<" no es Primo"<<endl;
+	}
+	
+	
+	return 0;
+}
+
---
+++ demo-esPrimo-raizCuadrada.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "esPrimo-raizCuadrada.h"
+using namespace std;
+
+int main(void) {
+	int n;
+	cout<<"Ingrese el numero que desea saber si es Primo: "<<endl;
+	cin>>n;
+	
+	int comprobar=esPrimo(n);
+	
+	if(comprobar==1){
+		cout<<"El numero "<<n<<" es Primo"<<endl;
+	}else{
+		cout<<"El numero "<<n<<" no es Primo"<<endl;
+	};
+	
+	return 0;
+}
+
