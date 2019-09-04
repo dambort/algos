3299e3c6c8d67f760f3ea74c380941f42d9fa903
MatiPalermo99 <51247617+MatiPalermo99@users.noreply.github.com>
2019-09-04 13:54:34 -0300

    Update demo-esPrimo-lineal.cpp

--- demo-esPrimo-lineal.cpp
+++ demo-esPrimo-lineal.cpp
@@ -2,6 +2,8 @@
 #include "esPrimo-lineal.h"
 using namespace std;
 
+int esPrimo(int n);
+
 int main (void) {
 	int n;
 	cout<<"Ingrese el numero que desea saber si es Primo: "<<endl;
@@ -20,3 +22,32 @@ int main (void) {
 	return 0;
 }
 
+int esPrimo(int n){
+	
+	//Recibimos un numero entero n el cual queremos
+	//saber si es primo
+	//la funcion retornará 1 si es el numero es primo, en caso
+	//contrario retornará -1
+	
+	bool esPrimo=true;
+	
+	//Seteamos un booleano, el cual
+	//cambiaremos si es necesario durante la ejecucion
+	
+	for(int i=2;i<n;i++){
+		if(n%i==0){
+			esPrimo=false;
+			break;
+		}
+	}
+	int retorno;
+	if(esPrimo){
+		//el numero n es primo
+		retorno=1;
+	}else{
+		//el numero n no es primo
+		retorno=-1;
+	}
+	
+	return retorno;
+}
