eca7b6a319f51e99b609865cd03e11716c98f107
Gonzalo Lopez <54336219+Gonzacap@users.noreply.github.com>
2019-09-04 17:46:29 -0300

    Add files via upload

---
+++ factorial-iterativo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+long long unsigned factorial(long long unsigned n)
+{
+	long long unsigned aux=1; //a aux se le asigna el valor del caso base (0!=1)
+	
+	for(int i=1; i<=n; i++){
+		// a aux se le asigna el factorial ya calculado (aux) multiplicado por n (valor de i)
+		aux= (aux)*i; // [n! = (n-1)! * n]
+	}
+	
+	n=aux;
+	return n;
+}
