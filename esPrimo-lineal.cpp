5cdc24d0b2178920f394947884d982bee8effa56
MatiPalermo99 <51247617+MatiPalermo99@users.noreply.github.com>
2019-09-11 13:16:47 -0300

    Update esPrimo-lineal.cpp

--- esPrimo-lineal.cpp
+++ esPrimo-lineal.cpp
@@ -5,20 +5,23 @@ int esPrimo(int n){
 	
 	//Recibimos un numero entero n el cual queremos
 	//saber si es primo
-	//la funcion retornar· 1 si es el numero es primo, en caso
-	//contrario retornar· -1
+	//la funcion retornar√° 1 si es el numero es primo, en caso
+	//contrario retornar√° -1
 	
 	bool esPrimo=true;
 	
 	//Seteamos un booleano, el cual
 	//cambiaremos si es necesario durante la ejecucion
 	
-	for(int i=2;i<n;i++){
-		if(n%i==0){
-			esPrimo=false;
-			break;
-		}
+	int i=2;
+	while((n%i!=0)&&(i<n)){
+		i++;
 	}
+	if(i!=n){
+		esPrimo=false;
+	}
+	
+	
 	int retorno;
 	if(esPrimo){
 		//el numero n es primo
