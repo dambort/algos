a99c4e4f6ffd06b0d4c63f2652d56ff501426165
Daniel Ambort <dambort@gmail.com>
2019-09-26 14:03:14 -0300

    Update añobisiesto.cpp

--- añobisiesto.cpp
+++ añobisiesto.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int aniobisiesto(int Anio){
-	// El programa pregunta si el año ingresado es divisible por 4 y despues si el año es divisible o por 100 o 400.
+	// La funcion pregunta si el aÃ±o ingresado es divisible por 4 y despues si no es divisible por 100 o es divisible por 400.
 	// Y en caso de cumplir esa condicion retorna 1.
 	if(Anio%4==0 && (Anio%100!=0 || Anio%400==0))
 		return 1;
