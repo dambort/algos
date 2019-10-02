01efb97b3e387709d130dd40e7d1b6d2de68358c
MatiPalermo99 <51247617+MatiPalermo99@users.noreply.github.com>
2019-10-02 14:35:27 -0300

    Add files via upload

---
+++ ordenamientoSeleccion.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void seleccion(int vector[TAM],int longitud){
+	int min;
+	//El algoritmo consiste en pararse en cada posicion del arreglo y desde
+	//ese lugar buscar la menor en las restantes posiciones, si existe se intercambia
+	//con la posicion actual, en otro caso, se pasa a la siguiente posicion y se 
+	//repite
+	for(int i=0;i<longitud;i++){
+		min=i;
+		for(int j=i;j<longitud;j++){
+			
+			if(vector[j]<vector[min]){
+				min=j;
+			}
+			
+		}
+		int aux=vector[i];
+		vector[i]=vector[min];
+		vector[min]=aux;
+		
+	}
+	
+	
+	
+}
+
